83c641116a5b41018e402bc60b203378ce4f6c3d
TonyAdo <coolhzb@163.com>
2014-12-04 10:58:22 +0800

    add the connection&rquest level configuration....

--- src/main/java/com/xqbase/java/AsyncClient.java
+++ src/main/java/com/xqbase/java/AsyncClient.java
@@ -16,15 +16,19 @@ import java.util.Arrays;
 
 public class AsyncClient implements Closeable {
 
-    public static final int DEFAULT_MAX_CONNECTIONS = 10;
+    public static final int DEFAULT_MAX_CONNECTIONS_PER_ROUTE = 10;
+    private static final int MAX_CONN_MULTIPLICATION = 10;
     public static final int DEFAULT_SOCKET_TIMEOUT = 10 * 1000;
+    public static final int DEFAULT_CONNECTION_REQUEST_TIMEOUT = 10 * 1000;
+    public static final int DEFAULT_CONNECT_TIMEOUT = 10 * 1000;
     public static final int DEFAULT_BUFFER_SIZE = 8192;
     public static final int DEFAULT_MAX_RETRIES = 5;
     public static final int DEFAULT_RETRY_SLEEP_TIME_MILLIS = 1500;
 
-    private int maxConnections = DEFAULT_MAX_CONNECTIONS;
-    private int connectTimeout = DEFAULT_SOCKET_TIMEOUT;
-    private int responseTimeout = DEFAULT_SOCKET_TIMEOUT;
+    private int connectTimeout = DEFAULT_CONNECT_TIMEOUT;
+    private int socketTimeout = DEFAULT_SOCKET_TIMEOUT;
+    private int connectionRequestTimeout = DEFAULT_CONNECTION_REQUEST_TIMEOUT;
+    private int defaultMaxPerRoute = DEFAULT_MAX_CONNECTIONS_PER_ROUTE;
 
     private final CloseableHttpAsyncClient httpAsyncClient;
     private PoolingNHttpClientConnectionManager connManager;
@@ -34,14 +38,16 @@ public class AsyncClient implements Closeable {
         // Create I/O reactor configuration
         IOReactorConfig ioReactorConfig = IOReactorConfig.custom()
                 .setIoThreadCount(Runtime.getRuntime().availableProcessors())
-                .setConnectTimeout(connectTimeout)
-                .setSoTimeout(responseTimeout)
+                .setTcpNoDelay(true)
                 .build();
 
         // Create a custom I/O reactor
         ConnectingIOReactor ioReactor = new DefaultConnectingIOReactor(ioReactorConfig);
 
+        // Create a custom Connection Manager
         connManager = new PoolingNHttpClientConnectionManager(ioReactor);
+        connManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
+        connManager.setMaxTotal(defaultMaxPerRoute * MAX_CONN_MULTIPLICATION);
 
         // Create global request configuration
         RequestConfig defaultRequestConfig = RequestConfig.custom()
@@ -57,18 +63,98 @@ public class AsyncClient implements Closeable {
                 .build();
     }
 
-    public int getMaxConnections() {
-        return maxConnections;
+    /**
+     * Get maximum connections per route allowed
+     * @return maximum connections per route allowed
+     */
+    public int getDefaultMaxPerRoute() {
+        return defaultMaxPerRoute;
     }
 
-    public void setMaxConnections(int maxConnections) {
-        maxConnections = maxConnections < 1 ? DEFAULT_MAX_CONNECTIONS : maxConnections;
-        this.maxConnections = maxConnections;
-        connManager.setMaxTotal(maxConnections);
+    /**
+     * Determines the maximum connections per route allowed
+     * @param defaultMaxPerRoute maximum connections value
+     */
+    public void setDefaultMaxPerRoute(int defaultMaxPerRoute) {
+        defaultMaxPerRoute = defaultMaxPerRoute < 1 ? DEFAULT_MAX_CONNECTIONS_PER_ROUTE : defaultMaxPerRoute;
+        this.defaultMaxPerRoute = defaultMaxPerRoute;
+        this.connManager.setDefaultMaxPerRoute(this.defaultMaxPerRoute);
+        this.connManager.setMaxTotal(this.defaultMaxPerRoute * MAX_CONN_MULTIPLICATION);
+    }
+
+    /**
+     * Determines the timeout in milliseconds until a connection is established.
+     * A timeout value of zero is interpreted as an infinite timeout.
+     * <p>
+     * A timeout value of zero is interpreted as an infinite timeout.
+     * A negative value is interpreted as undefined (system default).
+     * </p>
+     * <p>
+     * Default: {@code -1}
+     * </p>
+     */
+    public int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    /**
+     * Set the connection timeout
+     * @param connectTimeout connect timeout
+     */
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    /**
+     * Defines the socket timeout ({@code SO_TIMEOUT}) in milliseconds,
+     * which is the timeout for waiting for data  or, put differently,
+     * a maximum period inactivity between two consecutive data packets).
+     * <p>
+     * A timeout value of zero is interpreted as an infinite timeout.
+     * A negative value is interpreted as undefined (system default).
+     * </p>
+     * <p>
+     * Default: {@code -1}
+     * </p>
+     */
+    public int getSocketTimeout() {
+        return socketTimeout;
+    }
+
+    /**
+     * Set the socket timeout
+     * @param socketTimeout socket timeout
+     */
+    public void setSocketTimeout(int socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    /**
+     * Returns the timeout in milliseconds used when requesting a connection
+     * from the connection manager. A timeout value of zero is interpreted
+     * as an infinite timeout.
+     * <p>
+     * A timeout value of zero is interpreted as an infinite timeout.
+     * A negative value is interpreted as undefined (system default).
+     * </p>
+     * <p>
+     * Default: {@code -1}
+     * </p>
+     */
+    public int getConnectionRequestTimeout() {
+        return connectionRequestTimeout;
+    }
+
+    /**
+     * Set the connection request timeout
+     * @param connectionRequestTimeout connection request timeout
+     */
+    public void setConnectionRequestTimeout(int connectionRequestTimeout) {
+        this.connectionRequestTimeout = connectionRequestTimeout;
     }
 
     @Override
     public void close() throws IOException {
-
+        httpAsyncClient.close();
     }
 }
